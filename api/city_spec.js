a15f5234d8f8a72b27f12d8145706d3d959a31ab
Asynchrony SPR-MD14 <SPR-MD14@asynchrony.com>
2017-08-16 20:10:01 -0500

    001. automate e2e testing endpoint .tp

--- api/city_spec.js
+++ api/city_spec.js
@@ -2,7 +2,7 @@ const frisby = require('frisby');
 
 describe('address city endpoint', function () {
   it('should return 200', function (done) {
-    frisby.get('http://localhost/address/city')
+    frisby.get('http://localhost:8000/address/city')
       .expect('status', 200)
       .done(done);
   });
