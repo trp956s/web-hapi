bbed8cfb0f91e56ca0a3c123b4dcde20435a9132
Springfield Shared Laptop 1 <spr-ml1@asynchrony.com>
2017-09-20 18:54:09 -0500

    003 - move db config file - tp

--- package-scripts/bootup-db.js
+++ package-scripts/bootup-db.js
@@ -3,7 +3,7 @@ let shutdownDb = require('./shutdown-db');
 
 let removeMySqlDataAndLogs = "rm -rf ./db";
 let createMySqlDataDirectory = "mkdir -p -m 777 ./db; mkdir -m 777 ./db/data";
-let runMongod = "mongod --config ./mongod.conf";
+let runMongod = "mongod --config ./package-scripts/mongod.conf";
 
 shell.exec(removeMySqlDataAndLogs);
 shell.exec(createMySqlDataDirectory);
