81279d56b29adce776149cef0f7e4305366a2ec4
Springfield Shared Laptop 1 <spr-ml1@asynchrony.com>
2017-10-18 00:45:21 -0500

    004.WIP add tests for server bootup .tp

--- api/server.test.js
+++ api/server.test.js
@@ -19,4 +19,29 @@ describe('server', ()=>{
 
         expect(connectionMock).toHaveBeenCalledWith('foo');
     });
+
+    it('should send the plugins to get registered', async ()=>{
+        const registerSpy = jest.fn();
+        const fakePluginList = [];
+        const fakeServer= {
+            connection:()=>{},
+            register: registerSpy
+        };
+        require('hapi').Server = jest.fn(()=>{
+            return fakeServer;
+        });
+        jest.doMock('./server/pluginList', ()=>()=>Promise.resolve(fakePluginList));
+
+        await require('./server');
+
+        expect(registerSpy).toHaveBeenCalledWith(fakePluginList);
+    });
+
+    it('should throw an error if the plugins fail to load', async ()=>{
+        expect(true).toBeFalsy('test not written');
+    });
+
+    it('should run the server AFTER loading all plugins', async ()=>{
+        expect(true).toBeFalsy('test not written.  also you should probalby rename onLoaded');
+    });
 });
