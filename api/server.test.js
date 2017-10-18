d33d5f86717186ada1b46ed21b3570331916460c
Springfield Shared Laptop 1 <spr-ml1@asynchrony.com>
2017-10-18 00:24:52 -0500

    004.WIP refactoring server.js not reflected in test .tp

--- api/server.test.js
+++ api/server.test.js
@@ -3,25 +3,20 @@ const Hapi = require('hapi');
 describe('server', ()=>{
     it('should set the connection', async ()=>{
         const connectionMock = jest.fn();
-        const registerMock = jest.fn();
-        const onLoadedMock = jest.fn(() => 'baz');
         const fakeServer= {
             connection:connectionMock,
-            register:registerMock
         };
         const serverConstructorMock = jest.fn(()=>{
             return fakeServer;
         });
 
         require('hapi').Server = serverConstructorMock;
-        jest.doMock('./server/connection', ()=>{return jest.fn(() => 'foo');});
-        jest.doMock('./server/pluginList', ()=>{return jest.fn(() => Promise.resolve('bar'));});
-        jest.doMock('./server/onLoaded', ()=>onLoadedMock);
+        jest.doMock('co', ()=>()=>{});
+        jest.doMock('./server/connection', ()=>()=>'foo');
+        jest.doMock('./server/onLoaded', ()=>()=>{});
 
         await require('./server');
 
         expect(connectionMock).toHaveBeenCalledWith('foo');
-        expect(registerMock).toHaveBeenCalledWith('bar', 'baz');
-        expect(onLoadedMock).toHaveBeenCalledWith(fakeServer);
     });
 });
